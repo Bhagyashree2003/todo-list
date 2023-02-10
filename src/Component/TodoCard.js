5d243b344d8fefa8a6a4cda987cb9e3828d60740
bhagyashree2003 <dungarwalmeena49@gmail.com>
2023-02-10 15:14:20 +0530

    first

---
+++ src/Component/TodoCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoItem from "./todoItem";
+
+function TodoCard() {
+  return (
+    <>
+      <TodoItem />
+    </>
+  );
+}
+
+export default TodoCard;
---
+++ src/Component/TodoCardItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TodoCard from "./TodoCard";
+
+function TodoCardItem() {
+  return (
+    <>
+      <TodoCard />
+    </>
+  );
+}
+
+export default TodoCardItem;
